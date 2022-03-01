fe059b947ff58318387120a718a6af5ba0849cc8
Benjamin Steinwender <b@stbe.at>
2022-03-01 20:10:57 +0100

    always pull

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 tag=$(date +"%Y-%m-%d")
 echo Tag: $tag
-docker build --no-cache -t steinweb/fedora-latex:$tag .
+docker build --no-cache -t steinweb/fedora-latex:$tag --pull .
 docker tag steinweb/fedora-latex:$tag steinweb/fedora-latex:latest
 docker push steinweb/fedora-latex:$tag
 docker push steinweb/fedora-latex:latest
