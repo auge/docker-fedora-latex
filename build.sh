bbe654709a79df3fbf6b0245675ba69e8af25ad9
Benjamin Steinwender <auge@users.noreply.github.com>
2022-07-18 09:20:02 +0200

    Update build.sh
    removed empty line

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ docker build --no-cache -t steinweb/fedora-latex:$tag --pull .
 docker tag steinweb/fedora-latex:$tag steinweb/fedora-latex:latest
 docker push steinweb/fedora-latex:$tag
 docker push steinweb/fedora-latex:latest
-
