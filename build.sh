7d584bd7855f006004065fc5b738e304dd3c8a8f
Benjamin Steinwender <b@stbe.at>
2023-01-31 22:57:27 +0100

    support other machines

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #/usr/bin/env sh
 
+l=latest
 tag=$(date +"%Y-%m-%d")
+m=$(uname -m)
+if [ $m != x86_64 ]; then
+    tag=$tag-$m
+    l=$l-$m
+fi
 echo Tag: $tag
 docker build --no-cache -t steinweb/fedora-latex:$tag --pull .
-docker tag steinweb/fedora-latex:$tag steinweb/fedora-latex:latest
+docker tag steinweb/fedora-latex:$tag steinweb/fedora-latex:$l
 docker push steinweb/fedora-latex:$tag
-docker push steinweb/fedora-latex:latest
+docker push steinweb/fedora-latex:$l
